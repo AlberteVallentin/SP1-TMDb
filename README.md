911d96f8b1eecf44d942c66bd613fd919df0c394
AlberteVallentin <143842711+AlberteVallentin@users.noreply.github.com>
2024-09-17 09:32:56 +0200

    setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Startcode for Java Deep Dive
+
+Includes:
+
+- Intellij
+- JPA dependencies
+- Lombok
+- Junit 5
+- Postgres drivers
+- Test containers
+
+Is created for Java 17 corretto
+
+The person entity is just for testing purposes and needs a database called "person" to work with JPA.
---
+++ src/main/java/dat/entities/Person.java
@@ -0,0 +1,24 @@
+package dat.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+}
